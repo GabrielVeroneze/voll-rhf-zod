9c81353c7cfb15232aef41150ee0aef12b67f298
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-09-20 14:04:42 -0300

    ✨ feat: cria `cadastroEspecialistaEnderecoSchema`
    Adiciona schema de validação para endereço do especialista, incluindo campos como CEP, rua, número, bairro e localidade. Também criado o tipo `CadastroEspecialistaEnderecoSchemaType` para uso tipado no formulário.

---
+++ src/schemas/cadastroEspecialistaEnderecoSchema.ts
@@ -0,0 +1,24 @@
+import { z } from 'zod'
+
+export const cadastroEspecialistaEnderecoSchema = z.object({
+    endereco: z.object({
+        cep: z
+            .string()
+            .min(9, 'Informe um CEP válido'),
+        rua: z
+            .string()
+            .min(1, 'Informe uma rua válida'),
+        numero: z
+            .coerce
+            .number<number>()
+            .min(1, 'Informe um número válido'),
+        bairro: z
+            .string()
+            .min(1, 'Informe um bairro válido'),
+        localidade: z
+            .string()
+            .min(1, 'Informe uma localidade válida'),
+    }),
+})
+
+export type CadastroEspecialistaEnderecoSchemaType = z.infer<typeof cadastroEspecialistaEnderecoSchema>
